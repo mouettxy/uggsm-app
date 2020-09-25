9239e5fa3f1055cc99ea4c0f11af54492135b2d7
newfox79 <znaykaznaykovich@gmail.com>
2020-09-25 22:02:13 +0300

    feat(atom): add atom prototype that switches offices

---
+++ uggsm-client/src/components/moleculas/MOfficeSwitcher.vue
@@ -0,0 +1,43 @@
+<template lang="pug">
+.office-switcher
+  v-select(
+    v-model='model',
+    :items='items',
+    dense,
+    outlined,
+    menu-props='auto',
+    label='Офис',
+    hide-details,
+    single-line,
+    no-data-text='Похоже нет доступных офисов...'
+  )
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+/**
+ * Atom that switch offices
+ *
+ * @emits change
+ *
+ */
+@Component
+export default class MOfficeSwitcher extends Vue {
+  @Prop(Array) items: any
+  @Prop(String) value: any
+
+  get model() {
+    return this.value
+  }
+
+  set model(value) {
+    /**
+     * Fires when office changes
+     *
+     * @property {string} value - current office
+     */
+    this.$emit('change', value)
+  }
+}
+</script>
