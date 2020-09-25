2be719ac1ada7fa4362306782547143f165111a1
newfox79 <znaykaznaykovich@gmail.com>
2020-09-25 22:06:51 +0300

    feat(templates): add template for order page

---
+++ uggsm-client/src/templates/TOrdersMain.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+.orders-main
+  o-table-toolbar
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+import OTableToolbar from '@/components/organizms/OTableToolbar.vue'
+
+/**
+ * Template of main order view
+ *
+ */
+@Component({
+  components: {
+    OTableToolbar
+  }
+})
+export default class TOrdersMain extends Vue {}
+</script>
