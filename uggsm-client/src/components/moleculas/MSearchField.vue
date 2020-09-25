090c8fc750a2e3ad1b37d7cd837aba08ac5ee896
newfox79 <znaykaznaykovich@gmail.com>
2020-09-25 22:02:42 +0300

    feat(atom): add search field prototype that provide default look for search field

---
+++ uggsm-client/src/components/moleculas/MSearchField.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+.search-field
+  v-text-field(
+    v-model='model',
+    outlined,
+    clearable,
+    dense,
+    hide-details,
+    single-line,
+    prepend-inner-icon='mdi-magnify',
+    label='Поиск'
+  )
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+/**
+ * Atom that provides cool look search field
+ *
+ * @emits change
+ */
+@Component
+export default class MSearchField extends Vue {
+  public value = ''
+
+  get model() {
+    return this.value
+  }
+
+  set model(value: string) {
+    /**
+     * Emits when search changes
+     *
+     * @property {string} value - input value
+     */
+    this.$emit('change', value)
+  }
+}
+</script>
