ae49ba375f014f2451c050589b731f9bb320b5fc
newfox79 <znaykaznaykovich@gmail.com>
2020-10-04 16:04:42 +0300

    feat(atom-right-modal): add right modal component

---
+++ uggsm-client/src/components/atoms/ARightModal.vue
@@ -0,0 +1,59 @@
+<template lang="pug">
+.right-modal
+  slot(name='activator', :click='toggleModal')
+  v-dialog(
+    v-model='value',
+    persistent,
+    transition='dialog-bottom-transition',
+    overlay-opacity='0.8',
+    content-class='right-modal'
+  )
+    v-card.card
+      slot(name='content', :close='toggleModal')
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator'
+
+@Component
+export default class ARightModal extends Vue {
+  value = false
+  @Prop(String) title: any
+  @Prop(Boolean) active: any
+
+  public interval = setInterval(() => {
+    if (this.active) {
+      document.documentElement.classList.add('overflow-y-hidden')
+    } else {
+      document.documentElement.classList.remove('overflow-y-hidden')
+    }
+  }, 100)
+
+  toggleModal() {
+    this.value = !this.value
+  }
+
+  mounted() {
+    if (this.active) {
+      this.value = true
+    }
+  }
+
+  beforeDestroy() {
+    clearInterval(this.interval)
+    document.documentElement.classList.remove('overflow-y-hidden')
+  }
+}
+</script>
+
+<style lang="sass">
+.v-dialog.right-modal
+  position: absolute
+  height: 100vh
+  width: 65vw !important
+  top: 0
+  left: 35vw
+  border-radius: 0px !important
+  max-height: 100%
+  margin: 0
+</style>
