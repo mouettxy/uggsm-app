6198b5f3f44b00fffd666ac9636b33d0990d58fa
newfox79 <znaykaznaykovich@gmail.com>
2020-10-04 16:10:29 +0300

    feat(template-orders): add table to template

--- uggsm-client/src/templates/TOrdersMain.vue
+++ uggsm-client/src/templates/TOrdersMain.vue
@@ -1,20 +1,23 @@
 <template lang="pug">
 .orders-main
   o-table-toolbar
+  .orders-main__content.pa-4
+    o-orders-table
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 
 import OTableToolbar from '@/components/organizms/OTableToolbar.vue'
-
+import OOrdersTable from '@/components/organizms/OOrdersTable.vue'
 /**
  * Template of main order view
  *
  */
 @Component({
   components: {
-    OTableToolbar
+    OTableToolbar,
+    OOrdersTable
   }
 })
 export default class TOrdersMain extends Vue {}
