af9a612c7cb11eb48ea555fb6106baf35ff59dcf
newfox79 <znaykaznaykovich@gmail.com>
2020-10-04 16:03:36 +0300

    feat(notifications): add z-index to notifications

--- uggsm-client/src/App.vue
+++ uggsm-client/src/App.vue
@@ -9,3 +9,8 @@ import { Component, Vue } from 'vue-property-decorator'
 @Component
 export default class App extends Vue {}
 </script>
+
+<style lang="sass">
+.notificationCenter.topRight
+  z-index: 999999
+</style>
