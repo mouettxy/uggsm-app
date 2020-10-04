319b367cbacc12d4de66d62f88db326a33b0e2af
newfox79 <znaykaznaykovich@gmail.com>
2020-10-04 16:06:47 +0300

    refactor(*): add index file imports from moleculas and atoms

---
+++ uggsm-client/src/components/atoms/index.ts
@@ -0,0 +1,15 @@
+import _ACenteredModal from './ACenteredModal.vue'
+import _AInput from './AInput.vue'
+import _ASelect from './ASelect.vue'
+import _ARightModal from './ARightModal.vue'
+import _AAutocomplete from './AAutocomplete.vue'
+import _ADatetimePicker from './ADatetimePicker.vue'
+import _ASwitch from './ASwitch.vue'
+
+export const AInput = _AInput
+export const ACenteredModal = _ACenteredModal
+export const ASelect = _ASelect
+export const ARightModal = _ARightModal
+export const AAutocomplete = _AAutocomplete
+export const ADatetimePicker = _ADatetimePicker
+export const ASwitch = _ASwitch
---
+++ uggsm-client/src/components/moleculas/index.ts
@@ -0,0 +1,7 @@
+import _MOfficeSwitcher from './MOfficeSwitcher.vue'
+import _MOrderModal from './MOrderModal/MOrderModal.vue'
+import _MSearchField from './MSearchField.vue'
+
+export const MOfficeSwitcher = _MOfficeSwitcher
+export const MOrderModal = _MOrderModal
+export const MSearchField = _MSearchField
