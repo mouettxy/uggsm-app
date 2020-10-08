645bc3cdc40565369a622562d2db95b7d4a47d12
newfox79 <znaykaznaykovich@gmail.com>
2020-10-09 02:42:23 +0300

    feat(database): try to reconnect on diconnect

--- uggsm-server/src/utils/databaseConnector.ts
+++ uggsm-server/src/utils/databaseConnector.ts
@@ -16,21 +16,21 @@ export function initializePlugins(_connection): void {
   // do nothing right now
 }
 
+export function connectWithRetry(uri) {
+  return mongoose.connect(uri, function (err) {
+    if (err) {
+      console.error('Failed to connect to mongo on startup - retrying in 5 sec')
+      setTimeout(() => {
+        connectWithRetry(uri)
+      }, 5000)
+    } else {
+      console.log('MongoDB connected succesefuly')
+    }
+  })
+}
+
 export function connectToDatabase(): void {
-  const DB_URL = `${DB_CONNECTION_STRING}${MONGO_USER}:${MONGO_PASSWORD}${DB_SERVER}`
-
-  mongoose
-    .connect(DB_URL)
-    .then((connection) => {
-      // tslint:disable-next-line: no-console
-      console.log('Database connected successfully')
-
-      initializePlugins(mongoose.connection)
-
-      console.log('Database plugins initialized')
-    })
-    .catch((error: Error) => {
-      // tslint:disable-next-line: no-console
-      console.log('Database connection error: ' + error.message)
-    })
+  const DB_URL = `${DB_CONNECTION_STRING}${MONGO_USER}:${MONGO_PASSWORD}${DB_SERVER}?connectTimeoutMS=1000&bufferCommands=false&authSource=admin`
+  console.log(DB_URL)
+  connectWithRetry(DB_URL)
 }
