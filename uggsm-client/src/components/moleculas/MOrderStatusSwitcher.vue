63daa1c040d8d5594f64527af4a9ce739ac3593e
newfox79 <znaykaznaykovich@gmail.com>
2020-10-19 02:47:44 +0300

    feat(statusSwitcher): change behavior on status 'Закрыт'

--- uggsm-client/src/components/moleculas/MOrderStatusSwitcher.vue
+++ uggsm-client/src/components/moleculas/MOrderStatusSwitcher.vue
@@ -1,6 +1,8 @@
 <template lang="pug">
 v-menu.order-status-switcher(
   :close-on-content-click='false',
+  min-width='200px',
+  max-height='300px',
   bottom
 )
   template(v-slot:activator='{ on, attrs }')
@@ -37,9 +39,9 @@ v-menu.order-status-switcher(
 
 <script lang="ts">
 import { Component, Vue, Prop } from 'vue-property-decorator'
-import { filter, each } from 'lodash'
+import { filter, each, reduce } from 'lodash'
 import { getCorrectTextColor } from '@/api/helpers'
-import { ordersModule } from '@/store'
+import { ordersModule, authModule, cashModule } from '@/store'
 import { ordersAPI } from '@/api'
 
 @Component
@@ -48,98 +50,114 @@ export default class MOrderStatusSwitcher extends Vue {
   @Prop({ default: 'modal', type: String }) scope!: string
   @Prop({ type: [String, Number] }) orderid!: string | number
 
-  public statusList = [
-    {
-      text: 'Новые',
-      statuses: [
+  get statusList() {
+    if (this.status === 'Закрыт' && authModule.user.role !== 'administrator') {
+      return [
         {
-          color: '#1858a1',
-          status: 'Отремонтирован',
+          text: 'Закрытые успешно',
+          statuses: [
+            {
+              color: '#626262',
+              status: 'Закрыт',
+            },
+          ],
         },
-        {
-          color: '#1858a1',
-          status: 'Новый',
-        },
-      ],
-    },
-    {
-      text: 'На исполнении',
-      statuses: [
-        {
-          color: '#689f38',
-          status: 'В работе',
-        },
-        {
-          color: '#689f38',
-          status: 'На тестировании',
-        },
-        {
-          color: '#689f38',
-          status: 'На уточнении',
-        },
-      ],
-    },
-    {
-      text: 'Отложенные',
-      statuses: [
-        {
-          color: '#FB8C00',
-          status: 'Позвонить повторно',
-        },
-        {
-          color: '#FB8C00',
-          status: 'Ждёт запчасть',
-        },
-        {
-          color: '#FB8C00',
-          status: 'Нужно решить',
-        },
-      ],
-    },
-    {
-      text: 'Готовые',
-      statuses: [
-        {
-          color: '#525252',
-          status: 'Готов',
-        },
-        {
-          color: '#525252',
-          status: 'Готов, без ремонта',
-        },
-        {
-          color: '#525252',
-          status: 'На продаже',
-        },
-      ],
-    },
-    {
-      text: 'Закрытые успешно',
-      statuses: [
-        {
-          color: '#626262',
-          status: 'Закрыт',
-        },
-        {
-          color: '#626262',
-          status: 'Выкуплен СЦ',
-        },
-        {
-          color: '#626262',
-          status: 'Обещали найти',
-        },
-      ],
-    },
-    {
-      text: 'Закрытые неуспешно',
-      statuses: [
-        {
-          color: '#b9b9b9',
-          status: 'Закрыт с вопросом',
-        },
-      ],
-    },
-  ]
+      ]
+    }
+
+    return [
+      {
+        text: 'Новые',
+        statuses: [
+          {
+            color: '#1858a1',
+            status: 'Отремонтирован',
+          },
+          {
+            color: '#1858a1',
+            status: 'Новый',
+          },
+        ],
+      },
+      {
+        text: 'На исполнении',
+        statuses: [
+          {
+            color: '#689f38',
+            status: 'В работе',
+          },
+          {
+            color: '#689f38',
+            status: 'На тестировании',
+          },
+          {
+            color: '#689f38',
+            status: 'На уточнении',
+          },
+        ],
+      },
+      {
+        text: 'Отложенные',
+        statuses: [
+          {
+            color: '#FB8C00',
+            status: 'Позвонить повторно',
+          },
+          {
+            color: '#FB8C00',
+            status: 'Ждёт запчасть',
+          },
+          {
+            color: '#FB8C00',
+            status: 'Нужно решить',
+          },
+        ],
+      },
+      {
+        text: 'Готовые',
+        statuses: [
+          {
+            color: '#525252',
+            status: 'Готов',
+          },
+          {
+            color: '#525252',
+            status: 'Готов, без ремонта',
+          },
+          {
+            color: '#525252',
+            status: 'На продаже',
+          },
+        ],
+      },
+      {
+        text: 'Закрытые успешно',
+        statuses: [
+          {
+            color: '#626262',
+            status: 'Закрыт',
+          },
+          {
+            color: '#626262',
+            status: 'Выкуплен СЦ',
+          },
+          {
+            color: '#626262',
+            status: 'Обещали найти',
+          },
+        ],
+      },
+      {
+        text: 'Закрытые неуспешно',
+        statuses: [
+          {
+            color: '#b9b9b9',
+            status: 'Закрыт с вопросом',
+          },
+        ],
+      },
+    ]
+  }
 
   get order() {
     return ordersModule.currentOrder
@@ -147,6 +165,72 @@ export default class MOrderStatusSwitcher extends Vue {
 
   accessibleColor(hex: string) {
     return getCorrectTextColor(hex)
+  }
+
+  async setPayed(order: any) {
+    try {
+      if (order) {
+        const response = await ordersAPI(order.id).setPayed({ payed: !order.payed })
+
+        if (response) {
+          this.$notification.success('Успешно изменён статус оплаты заказа')
+          await ordersModule.getOrder(order.id)
+        } else {
+          this.$notification.error('[Клиент] Не удалось изменить статус оплаты заказа')
+        }
+      } else {
+        this.$notification.error('[Клиент] Не удалось изменить статус оплаты заказа')
+      }
+    } catch (error) {
+      this.$notification.error('[Сервер] ' + error.message)
+    }
+  }
+
+  async setCash() {
+    try {
+      let order
+      if (this.order) {
+        order = this.order
+      } else if (this.orderid) {
+        const order_ = await ordersAPI(this.orderid).getById()
+
+        if (order_.status === 200) {
+          order = order_.data
+        } else {
+          this.$notification.error(
+            '[Клиент] Не удалось получить заказ, попробуйте сменить статус заказа из редактирования заказа'
+          )
+          return Promise.reject(false)
+        }
+      }
+
+      const income = reduce(
+        order.statusWork,
+        (a, e) => {
+          a += e.price
+          return a
+        },
+        0
+      )
+
+      const payload = {
+        orderid: order.id,
+        client: order.customer._id,
+        cashier: authModule.user._id,
+        income,
+      }
+
+      const response = await cashModule.createCash(payload)
+
+      if (response) {
+        this.$notification.success(`Касса по заказу №${order.id} успешно закрыта`)
+        await this.setPayed(order)
+      } else {
+        this.$notification.error('[Клиент] Произошла ошбика при закрытии кассы')
+      }
+    } catch (error) {
+      this.$notification.error(`[Сервер] ${error.message}`)
+    }
   }
 
   async setStatus(status: string) {
@@ -160,6 +244,11 @@ export default class MOrderStatusSwitcher extends Vue {
 
       if (response) {
         this.$notification.success('Успешная смена статуса')
+
+        if (status === 'Закрыт') {
+          await this.setCash()
+        }
+
         if (this.scope === 'modal') {
           await ordersModule.getOrder(this.order?.id)
         } else if (this.scope === 'table') {
@@ -190,9 +279,7 @@ export default class MOrderStatusSwitcher extends Vue {
 
 <style lang="sass">
 .order-status-switcher
-  &__lists
-    height: 300px
-    overflow-y: scroll
+  width: 250px !important
   &__list
     &-subheader
       height: 24px !important
