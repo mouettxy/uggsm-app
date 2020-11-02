c399be71a9021c21efdbe99ddd90e4854f1d486c
newfox79 <znaykaznaykovich@gmail.com>
2020-11-02 22:21:43 +0300

    feat(a-fullscreen-modal-2): init fullcreen modal that delegates v-model to caller

---
+++ uggsm-client/src/components/atoms/AFullscreenModal2.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+.fullscreen-modal-2
+  v-dialog(
+    v-model='model',
+    transition='dialog-bottom-transition',
+    fullscreen
+  )
+    template(#activator='{on, attrs}')
+      slot(
+        name='activator',
+        :on='on',
+        :attrs='attrs'
+      )
+    v-card
+      slot(name='content')
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator'
+
+@Component
+export default class AFullscreenModal extends Vue {
+  @Prop(Boolean) value!: boolean
+  @Prop(String) title!: string
+
+  get model() {
+    return this.value
+  }
+
+  set model(value) {
+    this.$emit('input', value)
+  }
+}
+</script>
