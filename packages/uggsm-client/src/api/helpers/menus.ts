be1ad248f9aea5d4d5431042f6dfcf5bf810e5fe
newfox79 <znaykaznaykovich@gmail.com>
2021-02-07 03:00:26 +0300

    feat(roles): implement access check based on role

--- packages/uggsm-client/src/api/helpers/menus.ts
+++ packages/uggsm-client/src/api/helpers/menus.ts
@@ -10,42 +10,49 @@ export const defaultMenu = [
     title: 'Заявки',
     icon: 'mdi-bat',
     divider: true,
+    resource: 'Order',
   },
   {
     linkName: 'clients',
     title: 'Клиенты',
     icon: 'mdi-account-group',
     divider: false,
+    resource: 'Client',
   },
   {
     linkName: 'cash',
     title: 'Касса',
     icon: 'mdi-cash-register',
     divider: false,
+    resource: 'Cash',
   },
   {
     linkName: 'calls',
     title: 'Звонки',
     icon: 'mdi-phone',
     divider: true,
+    resource: 'Calls',
   },
   {
     linkName: 'analytic',
     title: 'Аналитика',
     icon: 'mdi-google-analytics',
     divider: false,
+    resource: 'Analytics',
   },
   {
     linkName: 'reports',
     title: 'Отчёты',
     icon: 'mdi-filter',
     divider: true,
+    resource: 'Report',
   },
   {
     linkName: 'settings',
     title: 'Настройки',
     icon: 'mdi-cog',
     divider: true,
+    resource: 'Settings',
   },
 ]
 
@@ -55,18 +62,21 @@ export const reportsMenu = [
     title: 'Закрыто',
     icon: 'mdi-bat',
     divider: false,
+    resource: 'Report',
   },
   {
     linkName: 'reportsOrdersWithoutWork',
     title: 'Создано',
     icon: 'mdi-account-cancel',
     divider: false,
+    resource: 'Report',
   },
   {
     linkName: 'reportsOrdersCount',
     title: 'В работе',
     icon: 'mdi-counter',
     divider: false,
+    resource: 'Report',
   },
 ]
 
@@ -76,17 +86,20 @@ export const settingsMenu = [
     title: 'Новый пользователь',
     icon: 'mdi-account-plus',
     divider: false,
+    resource: 'Settings',
   },
   {
     linkName: 'settingsNewOffice',
     title: 'Новый офис',
     icon: 'mdi-office-building-marker',
     divider: true,
+    resource: 'Settings',
   },
   {
     linkName: 'settingsManageDailySubscriptions',
     title: 'Управление ежедневными отчётами',
     icon: 'mdi-calendar',
     divider: true,
+    resource: 'Settings',
   },
 ]
--- packages/uggsm-client/src/components/moleculas/MNavigationList.vue
+++ packages/uggsm-client/src/components/moleculas/MNavigationList.vue
@@ -4,17 +4,31 @@ v-list(
   dense
 )
   template(v-for='item in items')
-    template(v-if='item.divider')
-      v-divider.my-1
-    v-list-item(
-      :to='{ name: item.linkName }',
-      :key='item.title',
-      color='#fafafa'
-    )
-      v-list-item-icon
-        v-icon {{ item.icon }}
-      v-list-item-content
-        v-list-item-title {{ item.title }}
+    template(v-if='item.resource')
+      template(v-if='item.divider')
+        v-divider.my-1
+      v-list-item(
+        v-if='$can("access", item.resource)',
+        :to='{ name: item.linkName }',
+        :key='item.title',
+        color='#fafafa'
+      )
+        v-list-item-icon
+          v-icon {{ item.icon }}
+        v-list-item-content
+          v-list-item-title {{ item.title }}
+    template(v-else)
+      template(v-if='item.divider')
+        v-divider.my-1
+      v-list-item(
+        :to='{ name: item.linkName }',
+        :key='item.title',
+        color='#fafafa'
+      )
+        v-list-item-icon
+          v-icon {{ item.icon }}
+        v-list-item-content
+          v-list-item-title {{ item.title }}
 </template>
 
 <script lang="ts">
