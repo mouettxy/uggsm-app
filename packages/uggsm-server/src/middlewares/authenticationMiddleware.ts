61bd92897d0e7ebfb57967b6d7e87785d57d4565
newfox79 <znaykaznaykovich@gmail.com>
2021-02-08 12:47:50 +0300

    fix(auth): add additional check if token exists

--- packages/uggsm-server/src/middlewares/authenticationMiddleware.ts
+++ packages/uggsm-server/src/middlewares/authenticationMiddleware.ts
@@ -10,22 +10,26 @@ export async function authenticationMiddleware(
   response: Response,
   next: NextFunction
 ): Promise<void> {
-  const tokenData = request.headers.authorization.split(' ')
-  const tokenType = tokenData[0]
-  const token = tokenData[1]
-  if (tokenType === 'Bearer' && token) {
-    const secret = process.env.JWT_TOKEN_SECRET
-    try {
-      const verificationResponse = jwt.verify(token, secret) as AuthTokenData
-      const id = verificationResponse._id
-      const user = await UserModel.findById(id)
-      if (user) {
-        next()
-      } else {
+  if (request.headers.authorization) {
+    const tokenData = request.headers.authorization.split(' ')
+    const tokenType = tokenData[0]
+    const token = tokenData[1]
+    if (tokenType === 'Bearer' && token) {
+      const secret = process.env.JWT_TOKEN_SECRET
+      try {
+        const verificationResponse = jwt.verify(token, secret) as AuthTokenData
+        const id = verificationResponse._id
+        const user = await UserModel.findById(id)
+        if (user) {
+          next()
+        } else {
+          next(new WrongAuthenticationTokenException())
+        }
+      } catch (error) {
         next(new WrongAuthenticationTokenException())
       }
-    } catch (error) {
-      next(new WrongAuthenticationTokenException())
+    } else {
+      next(new AuthenticationTokenMissingException())
     }
   } else {
     next(new AuthenticationTokenMissingException())
