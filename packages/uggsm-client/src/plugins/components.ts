60b19693a3b4142e382fc43da5a1a7f1107efb30
newfox79 <znaykaznaykovich@gmail.com>
2021-02-18 22:45:38 +0300

    feat(components): add "ug-" prefix to new components

--- packages/uggsm-client/src/plugins/components.ts
+++ packages/uggsm-client/src/plugins/components.ts
@@ -1,13 +1,21 @@
+import { isNull, startsWith } from 'lodash'
+import { start } from 'repl'
 import Vue from 'vue'
 
 function requireComponents(req: any) {
   for (const key of req.keys()) {
     const name = /[\w,\s-]+\.(vue)$/g.exec(key)
 
-    console.log(name)
+    if (!isNull(name)) {
+      let componentName = name[0]
 
-    if (name) {
-      Vue.component(name[0].slice(0, -4), req(key).default)
+      if (startsWith(key, './base') || startsWith(key, './role')) {
+        componentName = 'ug-' + componentName
+      }
+
+      if (name) {
+        Vue.component(componentName.slice(0, -4), req(key).default)
+      }
     }
   }
 }
