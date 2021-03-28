de93645e6f19d97e1f12714eaf30780581b44408
newfox79 <znaykaznaykovich@gmail.com>
2021-03-28 23:10:43 +0300

    feat(modal-content): add sidebar and sidebar-mobile states

--- packages/uggsm-client/src/components/base/ui/modal-content/modal-content.vue
+++ packages/uggsm-client/src/components/base/ui/modal-content/modal-content.vue
@@ -1,56 +1,203 @@
 <template>
-  <div class="ug-modal-content">
-    <header class="ug-modal-content__header elevation-4">
-      <slot name="header"></slot>
+  <div
+    ref="sidebar"
+    class="ug-modal-content"
+    :class="{ 'ug-modal-content--sidebar': sidebar, 'ug-modal-content--mobile': isMobile }"
+  >
+    <header class="ug-modal-content__header">
+      <template v-if="sidebar && isMobile">
+        <div>
+          <slot name="header"></slot>
+        </div>
+        <ug-base-btn
+          class="ml-auto"
+          icon="mdi-comment-arrow-left"
+          color="dark"
+          @click="isSidebarVisible = !isSidebarVisible"
+        ></ug-base-btn>
+      </template>
+      <template v-else>
+        <slot name="header"></slot>
+      </template>
       <v-progress-linear :active="loading" bottom absolute indeterminate></v-progress-linear>
     </header>
-    <main class="ug-modal-content__main">
+    <main
+      class="ug-modal-content__main"
+      :class="{
+        'ug-modal-content__main--no-padding': mainNoPadding,
+        'ug-modal-content__main--no-scroll': mainNoScroll,
+      }"
+    >
       <slot name="main"></slot>
     </main>
     <footer class="ug-modal-content__footer">
       <slot name="footer"></slot>
     </footer>
+    <transition v-if="sidebar" name="ug-modal-content__sidebar--animation">
+      <aside v-if="isSidebarVisible" class="ug-modal-content__sidebar elevation-4">
+        <div class="ug-modal-content__sidebar-toolbar">
+          <slot name="sidebar-toolbar"></slot>
+          <ug-base-btn
+            v-if="isMobile"
+            class="ml-auto"
+            icon="mdi-comment-arrow-left"
+            color="dark"
+            @click="isSidebarVisible = !isSidebarVisible"
+          ></ug-base-btn>
+        </div>
+        <div class="ug-modal-content__sidebar-content">
+          <slot name="sidebar"></slot>
+        </div>
+      </aside>
+    </transition>
   </div>
 </template>
 
 <script>
+import UgBaseBtn from '@/components/base/ui/base-btn/base-btn'
+import Responsive from '@/mixins/responsive'
+
 export default {
   name: 'ug-modal-content',
+
+  components: {
+    UgBaseBtn,
+  },
+
+  mixins: [Responsive],
 
   props: {
     loading: {
       required: false,
       type: Boolean,
     },
+
+    mainNoPadding: {
+      required: false,
+      type: Boolean,
+    },
+
+    mainNoScroll: {
+      required: false,
+      type: Boolean,
+    },
+
+    sidebar: {
+      required: false,
+      type: Boolean,
+    },
+  },
+
+  data: () => ({
+    isSidebarVisible: true,
+  }),
+
+  watch: {
+    isMobile(value) {
+      if (this.sidebar) {
+        if (value) {
+          this.isSidebarVisible = false
+          return
+        }
+
+        this.isSidebarVisible = true
+      }
+    },
+  },
+
+  created() {
+    if (this.sidebar && this.isMobile) {
+      this.isSidebarVisible = false
+    }
   },
 }
 </script>
 
 <style lang="sass">
+
 .ug-modal-content
   width: 100%
   height: 100%
   display: flex
-  flex-direction: column
-  flex-wrap: nowrap
+  position: relative
+  flex-flow: column nowrap
 
-  .ug-modal-content__header
+  .ug-modal-content__main--no-padding
+    padding: 0 !important
+
+  .ug-modal-content__main--no-scroll
+    overflow: hidden !important
+
+  &.ug-modal-content--sidebar
+    padding-right: 350px
+
+    &.ug-modal-content--mobile
+      padding-right: 0px
+
+      .ug-modal-content__sidebar
+        width: 100%
+
+    .ug-modal-content__header
+      position: relative
+      display: flex
+      align-items: center
+      flex-shrink: 0
+      gap: 12px
+      padding: 12px
+
+    .ug-modal-content__main
+      flex-grow: 1
+      padding: 0 16px
+      overflow-y: auto
+      min-height: 1em
+
+    .ug-modal-content__footer
+      display: flex
+      flex-shrink: 0
+      gap: 12px
+      padding: 12px
+
+    .ug-modal-content__sidebar
+      position: fixed
+      top: 0
+      right: 0
+      padding: 12px
+      background: var(--v-table_lightgrey-base)
+      height: 100%
+      width: 350px
+
+      .ug-modal-content__sidebar-toolbar
+        display: flex
+        justify-content: space-between
+
+      .ug-modal-content__sidebar-content
+        height: 100%
+        overflow-y: auto
+
+  &:not(.ug-modal-content--sidebar) .ug-modal-content__header
     position: relative
     display: flex
     flex-shrink: 0
+    box-shadow: 0px 2px 4px -1px rgb(0 0 0 / 20%), 0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%) !important
     background: var(--v-table_lightgrey-base)
     gap: 12px
     padding: 12px
 
-  .ug-modal-content__main
+  &:not(.ug-modal-content--sidebar) .ug-modal-content__main
     flex-grow: 1
     padding: 0 16px
     overflow-y: auto
     min-height: 1em
 
-  .ug-modal-content__footer
+  &:not(.ug-modal-content--sidebar) .ug-modal-content__footer
     display: flex
     flex-shrink: 0
     gap: 12px
     padding: 12px
+
+.ug-modal-content__sidebar--animation-enter-active, .ug-modal-content__sidebar--animation-leave-active
+  transition: transform .3s
+
+.ug-modal-content__sidebar--animation-enter, .ug-modal-content__sidebar--animation-leave-to
+  transform: translateX(100%)
 </style>
