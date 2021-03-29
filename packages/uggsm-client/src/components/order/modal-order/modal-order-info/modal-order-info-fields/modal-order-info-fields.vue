c371ac56c15b466ed0225477aa48dbe72a278c3b
newfox79 <znaykaznaykovich@gmail.com>
2021-03-29 21:19:37 +0300

    feat(modal-order): init modal-order-info and related components

---
+++ packages/uggsm-client/src/components/order/modal-order/modal-order-info/modal-order-info-fields/modal-order-info-fields.vue
@@ -0,0 +1,209 @@
+<template>
+  <v-row>
+    <v-col cols="12" lg="6" md="6">
+      <ug-modal-client v-if="orderModel.customer && orderModel.customer.id" :clientid="orderModel.customer.id">
+        <template #activator="{ on, attrs }">
+          <span v-bind="attrs" v-on="on">
+            <v-icon size="1.3rem" color="table_darkergrey">mdi-pencil</v-icon>
+            <span class="text-subtitle-1 table_darkergrey--text">{{ orderModel.customerName }}</span>
+          </span>
+        </template>
+      </ug-modal-client>
+    </v-col>
+    <v-col cols="12" lg="6" md="6">
+      <span class="text-subtitle-1 table_darkergrey--text">
+        {{ orderModel.customerPhone | VMask('+7 (###) ###-##-##') }}
+      </span>
+    </v-col>
+    <v-col cols="12" lg="6" md="6">
+      <ug-base-input
+        v-model="orderModel.serialNumber"
+        :disabled="canEditGeneralFields"
+        :hide-details="true"
+        label="Серийный номер"
+        icon="mdi-fingerprint"
+      ></ug-base-input>
+    </v-col>
+    <v-col cols="12" lg="6" md="6">
+      <ug-base-input
+        v-model="orderModel.declaredPrice"
+        :disabled="canEditGeneralFields"
+        :hide-details="true"
+        type="number"
+        label="Ориентировочная цена"
+        icon="mdi-cash"
+      ></ug-base-input>
+    </v-col>
+    <v-col cols="12" lg="6" md="6">
+      <ug-base-autocomplete
+        v-model="orderModel.phoneBrand"
+        :predefined-items="orderModel.phoneBrand ? [{ text: orderModel.phoneBrand, value: orderModel.phoneBrand }] : []"
+        :hide-details="true"
+        :disabled="canEditGeneralFields"
+        label="Бренд"
+        icon="mdi-cellphone-information"
+        endpoint="/phone-brand"
+      ></ug-base-autocomplete>
+    </v-col>
+    <v-col cols="12" lg="6" md="6">
+      <ug-base-autocomplete
+        v-model="orderModel.phoneModel"
+        :predefined-items="orderModel.phoneModel ? [{ text: orderModel.phoneModel, value: orderModel.phoneModel }] : []"
+        :hide-details="true"
+        :disabled="canEditGeneralFields"
+        label="Модель"
+        icon="mdi-cellphone-information"
+        endpoint="/phone-model"
+      ></ug-base-autocomplete>
+    </v-col>
+    <v-col cols="12" lg="6" md="6">
+      <ug-base-autocomplete
+        v-model="orderModel.declaredDefect"
+        :predefined-items="
+          orderModel.declaredDefect ? [{ text: orderModel.declaredDefect, value: orderModel.declaredDefect }] : []
+        "
+        :hide-details="true"
+        :disabled="canEditGeneralFields"
+        label="Первичная неисправность"
+        icon="mdi-cellphone-erase"
+        endpoint="/declared-defect"
+      ></ug-base-autocomplete>
+    </v-col>
+    <v-col cols="12" lg="6" md="6">
+      <ug-base-autocomplete
+        v-model="orderModel.appearance"
+        :predefined-items="orderModel.appearance ? [{ text: orderModel.appearance, value: orderModel.appearance }] : []"
+        :hide-details="true"
+        :disabled="canEditGeneralFields"
+        label="Внешний вид"
+        icon="mdi-cellphone-text"
+        endpoint="/appearance"
+      ></ug-base-autocomplete>
+    </v-col>
+    <v-col cols="12" lg="6" md="6">
+      <ug-base-autocomplete
+        v-model="orderModel.kit"
+        :predefined-items="orderModel.kit ? [{ text: orderModel.kit, value: orderModel.kit }] : []"
+        :disabled="canEditGeneralFields"
+        label="Комплектация"
+        :hide-details="true"
+        icon="mdi-cellphone-cog"
+        endpoint="/kit"
+      ></ug-base-autocomplete>
+    </v-col>
+    <v-col cols="12" lg="6" md="6">
+      <ug-base-switch
+        v-model="orderModel.quick"
+        class="mt-0"
+        :disabled="canEditGeneralFields"
+        label="Срочно"
+        icon="mdi-alarm-light"
+        color="error"
+      ></ug-base-switch>
+    </v-col>
+    <v-col cols="12" lg="6" md="6">
+      <ug-base-autocomplete
+        v-model="orderModel.master._id"
+        :predefined-items="
+          orderModel.master ? [{ text: orderModel.master.credentials, value: orderModel.master._id }] : []
+        "
+        :disabled="isMasterFieldDisabled"
+        label="Мастер"
+        :hide-details="true"
+        icon="mdi-account-hard-hat"
+        endpoint="/master"
+        disallow-free-type
+      ></ug-base-autocomplete>
+    </v-col>
+    <v-col cols="12" lg="6" md="6">
+      <ug-base-autocomplete
+        v-model="orderModel.manager._id"
+        :predefined-items="
+          orderModel.manager ? [{ text: orderModel.manager.credentials, value: orderModel.manager._id }] : []
+        "
+        :disabled="isManagerFieldDisabled"
+        label="Менеджер"
+        :hide-details="true"
+        icon="mdi-account-cowboy-hat"
+        endpoint="/manager"
+        disallow-free-type
+      ></ug-base-autocomplete>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import UgModalClient from '@/components/client/modal-client/modal-client'
+import UgBaseAutocomplete from '@/components/base/ui/base-autocomplete/base-autocomplete'
+import UgBaseSwitch from '@/components/base/ui/base-switch/base-switch'
+import UgBaseInput from '@/components/base/ui/base-input/base-input'
+
+export default {
+  name: 'ug-modal-order-info-fields',
+
+  components: {
+    UgModalClient,
+    UgBaseAutocomplete,
+    UgBaseSwitch,
+    UgBaseInput,
+  },
+
+  props: {
+    order: {
+      required: false,
+      type: Object,
+      default: () => ({}),
+    },
+  },
+
+  computed: {
+    canEditGeneralFields() {
+      return !this.$can('editOrder', 'Global')
+    },
+
+    isMasterFieldDisabled() {
+      if (this.$can('editOrderMaster', 'Global')) {
+        return false
+      }
+
+      return true
+    },
+
+    isManagerFieldDisabled() {
+      if (this.$can('editOrderManager', 'Global')) {
+        return false
+      }
+
+      return true
+    },
+
+    orderModel: {
+      get() {
+        return this.order
+      },
+
+      set(value) {
+        this.$emit('update:order', value)
+      },
+    },
+  },
+
+  created() {
+    if (!this.orderModel.manager) {
+      this.orderModel.manager = {
+        _id: '',
+        credentials: 'НЕВЕРНОЕ ЗНАЧЕНИЕ',
+      }
+    }
+
+    if (!this.orderModel.master) {
+      this.orderModel.master = {
+        _id: '',
+        credentials: 'НЕВЕРНОЕ ЗНАЧЕНИЕ',
+      }
+    }
+  },
+}
+</script>
+
+<style></style>
---
+++ packages/uggsm-client/src/components/order/modal-order/modal-order-info/modal-order-info.vue
@@ -0,0 +1,131 @@
+<template>
+  <div class="modal-order-info">
+    <v-slide-x-transition>
+      <v-row v-if="hasMasterField">
+        <v-col cols="12">
+          <ug-base-alert class="mb-0" persistent type="error">
+            Не заполнено поле "Мастер", назначьте мастера для корректной работы с заявкой
+          </ug-base-alert>
+        </v-col>
+      </v-row>
+    </v-slide-x-transition>
+    <v-slide-x-transition>
+      <v-row v-if="hasManagerField">
+        <v-col cols="12">
+          <ug-base-alert class="mb-0" persistent type="error">
+            Не заполнено поле "Менеджер", назначьте менеджера для корректной работы с заявкой
+          </ug-base-alert>
+        </v-col>
+      </v-row>
+    </v-slide-x-transition>
+    <v-fade-transition>
+      <div v-if="modelLoaded">
+        <ug-modal-order-info-fields :order.sync="orderModel"></ug-modal-order-info-fields>
+      </div>
+      <v-row v-if="!modelLoaded">
+        <v-col cols="12" lg="6" md="6">
+          <v-skeleton-loader min-height="36" type="list-item"></v-skeleton-loader>
+        </v-col>
+        <v-col cols="12" lg="6" md="6">
+          <v-skeleton-loader min-height="36" type="list-item"></v-skeleton-loader>
+        </v-col>
+        <v-col cols="12" lg="6" md="6">
+          <v-skeleton-loader min-height="36" type="list-item"></v-skeleton-loader>
+        </v-col>
+        <v-col cols="12" lg="6" md="6">
+          <v-skeleton-loader min-height="36" type="list-item"></v-skeleton-loader>
+        </v-col>
+        <v-col cols="12" lg="6" md="6">
+          <v-skeleton-loader min-height="36" type="list-item"></v-skeleton-loader>
+        </v-col>
+        <v-col cols="12" lg="6" md="6">
+          <v-skeleton-loader min-height="36" type="list-item"></v-skeleton-loader>
+        </v-col>
+        <v-col cols="12" lg="6" md="6">
+          <v-skeleton-loader min-height="36" type="list-item"></v-skeleton-loader>
+        </v-col>
+        <v-col cols="12" lg="6" md="6">
+          <v-skeleton-loader min-height="36" type="list-item"></v-skeleton-loader>
+        </v-col>
+        <v-col cols="12" lg="6" md="6">
+          <v-skeleton-loader min-height="36" type="list-item"></v-skeleton-loader>
+        </v-col>
+        <v-col cols="12" lg="6" md="6">
+          <v-skeleton-loader min-height="36" type="list-item"></v-skeleton-loader>
+        </v-col>
+        <v-col cols="12" lg="6" md="6">
+          <v-skeleton-loader min-height="36" type="list-item"></v-skeleton-loader>
+        </v-col>
+        <v-col cols="12" lg="6" md="6">
+          <v-skeleton-loader min-height="36" type="list-item"></v-skeleton-loader>
+        </v-col>
+      </v-row>
+    </v-fade-transition>
+  </div>
+</template>
+
+<script>
+import UgModalOrderInfoFields from '@/components/order/modal-order/modal-order-info/modal-order-info-fields/modal-order-info-fields'
+import UgBaseAlert from '@/components/base/ui/base-alert/base-alert'
+
+export default {
+  name: 'ug-modal-order-info',
+
+  components: {
+    UgBaseAlert,
+    UgModalOrderInfoFields,
+  },
+
+  props: {
+    order: {
+      required: false,
+      type: Object,
+      default: () => ({}),
+    },
+  },
+
+  computed: {
+    modelLoaded() {
+      return this.order && !!Object.keys(this.order).length
+    },
+
+    hasMasterField() {
+      // while order is loading we dont show anything
+      if (!this.modelLoaded) {
+        return false
+      }
+
+      // but if we have loaded model and incoreect field we return truthy value
+      if (!(this.modelLoaded && this.orderModel.master?._id)) {
+        return true
+      }
+
+      return false
+    },
+
+    hasManagerField() {
+      // while order is loading we dont show anything
+      if (!this.modelLoaded) {
+        return false
+      }
+
+      // but if we have loaded model and incoreect field we return truthy value
+      if (!(this.modelLoaded && this.orderModel.manager?._id)) {
+        return true
+      }
+
+      return false
+    },
+
+    orderModel: {
+      get() {
+        return this.order
+      },
+
+      set(value) {
+        this.$emit('update:order', value)
+      },
+    },
+  },
+}
+</script>
