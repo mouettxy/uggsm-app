1b7c32cfe3e27be59d01317ced61bb9aba67192c
newfox79 <znaykaznaykovich@gmail.com>
2021-04-05 21:10:02 +0300

    feat: add office check

--- packages/uggsm-client/src/pages/settings/new-office.vue
+++ packages/uggsm-client/src/pages/settings/new-office.vue
@@ -25,6 +25,7 @@
           )
             ug-base-input(
               v-model.trim='office.name',
+              :single-line='false',
               :rules='requiredField',
               label='Название',
               icon='mdi-account'
@@ -36,6 +37,7 @@
           )
             ug-base-input(
               v-model.trim='office.code',
+              :single-line='false',
               :rules='requiredField',
               label='Код',
               icon='mdi-account'
@@ -44,6 +46,7 @@
           v-col(cols='12')
             ug-base-input(
               v-model.trim='office.address',
+              :single-line='false',
               :rules='requiredField',
               label='Адрес',
               icon='mdi-account'
@@ -56,7 +59,9 @@
           )
             ug-base-input(
               v-model.trim='office.ordersTemplate',
-              :rules='requiredField',
+              :single-line='false',
+              :rules='[...requiredField, ...officeTemplateCheck]',
+              @input='handleOrdersTemplateInput',
               label='Шаблон ID заявок',
               icon='mdi-account',
               hint='Например ```4{C:4}```'
@@ -68,11 +73,16 @@
           )
             ug-base-input(
               v-model.trim='office.docsTemplate',
+              :single-line='false',
               :rules='requiredField',
               label='Шаблон ID документа',
               icon='mdi-account',
               hint='Например ```BA{C:4}``` (Пока нигде не используется)'
             )
+        v-slide-y-transition
+          v-row(v-if='officeState')
+            v-col(cols='12')
+              .ug-markdown-content(v-md) {{ officeState }}
         v-row
           v-col(cols='12')
             ug-base-btn(
@@ -88,6 +98,8 @@ import OfficeAPI from '@/api/office'
 import UgBaseBtn from '@/components/base/ui/base-btn/base-btn'
 import UgBaseInput from '@/components/base/ui/base-input/base-input'
 
+const ORDER_TEMPLATE_REGEX = /(\d*)\{(C):(\d)\}/g
+
 export default {
   name: 'ug-settings-new-office',
 
@@ -102,6 +114,11 @@ export default {
     return {
       isFormValid: false,
       requiredField: [(v) => !!v || 'Необходимое поле'],
+      officeTemplateCheck: [
+        (v) => ORDER_TEMPLATE_REGEX.test(v) || 'Неверно заполнено поле. Примеры правильных шаблонов 4{C:4} 18{C:2}',
+      ],
+
+      officeState: '',
       office: {
         code: '',
         name: '',
@@ -121,6 +138,18 @@ export default {
         ordersTemplate: '',
         docsTemplate: '',
       }
+    },
+
+    handleOrdersTemplateInput() {
+      const isCorrect = ORDER_TEMPLATE_REGEX.test(this.office.ordersTemplate)
+
+      if (!isCorrect) {
+        this.officeState =
+          'Некорректное заполнение поля "Шаблон ID заявок".\n- Проверьте корректность цифры перед фигурными скобками **4**{C:*4*}\n- Проверьте что первый аргумент фигурных скобок буква C в английском регистре 4{**C**:4}\n- Проверьте что второй аргумент внутри фигурных скобок положительное число 4{C:**4**}'
+        return
+      }
+
+      this.officeState = ''
     },
 
     async createOffice() {
